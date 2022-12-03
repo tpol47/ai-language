942e1cd3c26cb659beab061a7de22b4e248fa507
Thomas Pollick <thomaspollick@Thomass-MacBook-Pro.local>
2022-12-03 13:35:36 -0600

    useeffects for translation and event listeners for language selection

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import captionsObj from './captions.js'
 
 
 function App() {
+  console.log('hello')
+  const caption = "The tall elf king and his cat look at the heavenly kingdom beyond the horizon"
   const [options, setOptions] = useState([])
   const [to, setTo] = useState("en");
   const [from, setFrom] = useState("en");
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(caption);
   const [output, setOutput] = useState("");
+  const [display, setDisplay] = useState(false);
   const translate = () => {
     const params = new URLSearchParams();
     params.append('q', input);
@@ -32,43 +35,55 @@ function App() {
     axios.get("https://libretranslate.de/languages", { headers: { 'accept': 'application/json' } }).then(res => {
       console.log(res)
       setOptions(res.data)
-    })
-  })
+    });
+    console.log('done')
+  }, [])
+
+  useEffect(() => {
+    setDisplay(false)
+    translate()
+    console.log('translated')
+  }, [to])
+
+  const handleChange = (e) => {
+    setTo(e.target.value) 
+  }
   return (
     <div className="App-container">
       <div>
-        <div>
-          From ({from}):
-          <select onChange={e => setFrom(e.target.value)}>
+        <div className="Select">
+          {/* From ({from}):
+          <select onChange={e => setFrom(e.target.value) }>
             {options.map(opt => <option key={opt.code} value={opt.code}>{opt.name}</option>)}
 
-          </select>
-          To({to}):
-          <select onChange={e => setTo(e.target.value)}>
+          </select> */}
+          Select language: ({to}):
+          <select onChange={e => handleChange(e)}>
             {options.map(opt => <option key={opt.code} value={opt.code}>{opt.name}</option>)}
 
           </select>
         </div>
-        <div>
+        {/* <div>
           <textarea cols='50' rows='8' onInput={(e) => setInput(e.target.value)}></textarea>
         </div>
 
         <div>
           <textarea cols='50' rows='8' value={output}></textarea>
-        </div>
+        </div> */}
 
-        <div>
+        {/* <div>
           <button onClick={e => translate()}>Translate</button>
-        </div>
+        </div> */}
 
       </div>
       );
       <div className="App" style={{ backgroundImage: `url(${background})` }}>
         <div className="Caption">
-          <div>El rey elfo alto y su gato miran el reino celestial más allá del horizonte</div>
-          <button>Translate</button>
+          {output}
           <br />
-          {captionsObj['elf-king.png']}
+          <button onClick={() => setDisplay(!display)}>Show translation</button>
+          <br />
+          {display && <div>{caption}</div>}
         </div>
       </div>
     </div>
